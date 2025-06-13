f275b6fbd4d862d0a2325c446c5c788846ae9f5f
azkaghafoor <azkaghafoor17@gmail.com>
2025-06-12 16:47:43 -0800

    conversion datatypes and operators practice done

---
+++ conversionOperations.js
@@ -0,0 +1,58 @@
+let score = "33";
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Azka" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+// ******************** Operations ****************
+let value = 3;
+let negvalue = -value;
+//console.log(negvalue);
+//console.log(2+2);  // Plus
+//console.log(2-2);  // Minus
+//console.log(2*2);  // Multiple
+//console.log(2**2);  // Power
+//console.log(2/2);   // Devide
+//console.log(2%2);   // Modulus => Reminder
+
+let str1 = "Hello"
+let str2 = " Azka"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log(2 + 3 * 4 % 2);
+
+//console.log(+true);
+//console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
